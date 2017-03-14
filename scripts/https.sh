6e4ff11292d97fc036225cdaaf7b68688cf8f542
Esa Firman <esafirm21@gmail.com>
2017-03-14 23:02:12 +0700

    Add SHA1 publichash variant

--- scripts/https.sh
+++ scripts/https.sh
@@ -10,3 +10,16 @@ publichash(){
 	openssl s_client -connect $1:$2 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 
 }
+
+publichash_sha1(){
+
+	# check arguments
+    if [ $# -eq 0 ]; 
+    then 
+        echo "No arguments specified. Use public hash [DOMAIN] [PORT]"
+        return 1
+    fi
+
+	openssl s_client -connect $1:$2 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha1 -binary | openssl enc -base64
+
+}
