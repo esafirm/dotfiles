b6302d86c3fbf738ce9da9936d0078d072af17ef
Esa Firman <esafirm21@gmail.com>
2017-03-14 21:19:18 +0700

    Add generate public hash for certification

---
+++ scripts/https.sh
@@ -0,0 +1,12 @@
+publichash(){
+
+	# check arguments
+    if [ $# -eq 0 ]; 
+    then 
+        echo "No arguments specified. Use public hash [DOMAIN] [PORT]"
+        return 1
+    fi
+
+	openssl s_client -connect $1:$2 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
+
+}
