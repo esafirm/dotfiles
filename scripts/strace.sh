62696f833c4f9c8609f257c6a3f52704c1434347
Esa Firman <esafirm21@gmail.com>
2019-07-03 13:26:26 +0700

    Add comment

--- scripts/strace.sh
+++ scripts/strace.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+## Running systrace in ANDROID_HOME
+## Result will be printed out in the stdout, if not please go to systrace dir
 strace() {
     SYSTRACE_DIR=$ANDROID_HOME/platform-tools/systrace
     cd $SYSTRACE_DIR
