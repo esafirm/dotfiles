e35adee9514daf7c96f6d19562852fb67b7d11e8
Esa Firman <esafirm21@gmail.com>
2019-07-03 13:25:42 +0700

    Fix strace scripts

--- scripts/strace.sh
+++ scripts/strace.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 strace() {
-    parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-    cd "$parent_path"
-
-    python2.7 systrace/systrace.py sched freq idle am wm gfx view binder_driver hal dalvik camera input res
+    SYSTRACE_DIR=$ANDROID_HOME/platform-tools/systrace
+    cd $SYSTRACE_DIR
+    python2.7 systrace.py sched freq idle am wm gfx view binder_driver hal dalvik camera input res
 }
