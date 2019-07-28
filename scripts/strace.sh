1d85902ddfaf8c053e4a3dfb82a2604710577e53
Esa Firman <esafirm21@gmail.com>
2019-07-28 19:53:18 +0700

    Update systrace script

--- scripts/strace.sh
+++ scripts/strace.sh
@@ -3,7 +3,20 @@
 ## Running systrace in ANDROID_HOME
 ## Result will be printed out in the stdout, if not please go to systrace dir
 strace() {
+    setupStrace
+    python2.7 systrace.py sched freq idle am wm gfx view binder_driver hal dalvik camera input res
+}
+
+strace-categories() {
+    setupStrace
+    python2.7 systrace.py --list-categories
+}
+
+setupStrace() {
     SYSTRACE_DIR=$ANDROID_HOME/platform-tools/systrace
     cd $SYSTRACE_DIR
-    python2.7 systrace.py sched freq idle am wm gfx view binder_driver hal dalvik camera input res
+}
+
+openTrace() {
+    open file:///Users/esafirm/Library/Android/sdk/platform-tools/systrace/trace.html
 }
