7f84c260d4cb6e7ec808465d36a975d5437c6c86
Esa Firman <esafirm21@gmail.com>
2020-02-02 09:24:20 +0700

    Fix git select

--- ruby/git_select.rb
+++ ruby/git_select.rb
@@ -6,9 +6,12 @@ require 'tty-prompt'
 
 git_command = ARGV[0]
 
+## Translate git command cause the limitation of git alias
+git_command = 'git branch --sort=-committerdate' if git_command == 'recent'
+
 abort('Empty git command passed') if git_command.nil?
 
-git_result = `git branch`.chomp.split("\n")
+git_result = `#{git_command}`.chomp.split("\n")
 
 abort('Invalid git command') if git_result.nil?
 
