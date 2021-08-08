8a7866c29213763f650f11d399480de31adae622
Esa Firman <esafirm21@gmail.com>
2021-08-08 10:18:15 +0700

    one configuration for intellij and android studio

--- android/intellij/checkIntellij.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/Library/Preferences/
-ls | grep "Idea"
--- android/studio/install.py
+++ android/studio/install.py
@@ -3,28 +3,57 @@
 import os
 import sys
 import shutil
+import glob
 from pathlib import Path
 
-if len(sys.argv) < 2:
-    print("./install.py [version] [variant]")
-    print("\nvariants: \n- prod (default)\n- preview")
-    sys.exit()
+# Choices prompt
+import inquirer
 
-FLAVOR = sys.argv[2] if len(sys.argv) == 3 else "prod"
-DIR_NAME = "AndroidStudioPreview" if FLAVOR == "preview" else "AndroidStudio"
+
+questions = [
+    inquirer.List(
+        'platform',
+        message="What IDE you want to check",
+        choices=["Android Studio", "IntelliJ"]
+    )
+]
+answers = inquirer.prompt(questions)
+
+# Determine the directory
+platform = answers['platform']
+if platform == 'Android Studio':
+    base_dir = str(Path.home()) + "/Library/Application Support/Google/"
+elif platform == 'IntelliJ':
+    base_dir = str(Path.home()) + "/Library/Application Support/JetBrains/"
+
+# Get path for choices
+path_choices = []
+for path in glob.glob(base_dir + "*"):
+    segments = path.split('/')
+    item = segments[len(segments) - 1]
+    path_choices.append(item)
+
+questions = [
+    inquirer.List(
+        'path',
+        message="In what directory you want to install this?",
+        choices=path_choices
+    )
+]
+answers = inquirer.prompt(questions)
 
 current_path = os.getcwd()
-studio_path = str(Path.home()) + "/Library/Application Support/Google/" + DIR_NAME + sys.argv[1] + "/"
+install_path = base_dir + answers['path'] + "/"
 
-print("Path: " + studio_path)
+print("Path: " + install_path)
 answer = input("Is this the right path? Y/n:  ")
 
 if answer != "Y":
     print("Run again if you have the right path")
     sys.exit()
 
-if os.path.exists(studio_path) == False:
-    print("Android Studio path not exist")
+if os.path.exists(install_path) == False:
+    print("IDE path not exist")
     sys.exit()
 
 files = []
@@ -35,15 +64,15 @@ for file in os.listdir(current_path):
 print("Files ready to be replaced: {}".format(len(files)))
 
 for file in files:
-    studio_file = studio_path + file
+    studio_file = install_path + file
     current_file = current_path + "/" + file
 
-    # Removing files in studio dir
+    # Removing files in IDE dir
     if os.path.exists(studio_file):
         shutil.rmtree(studio_file)
         print("Removing: {}".format(studio_file))
 
-    # Copy dotfiles to studio dir
+    # Copy dotfiles to IDE dir
     os.symlink(current_file, studio_file)
     print("Create link for: {}".format(current_file))
 
