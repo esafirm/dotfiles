6b31bbabb19869006709c0f1d92bec64f2ad9ee1
Esa Firman <esafirm21@gmail.com>
2023-04-07 10:40:42 +0700

    new file template and change optimize import keystroke

---
+++ android/studio/fileTemplates/Kotlin Composable Function.kt
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+import androidx.compose.runtime.Composable
+
+#end
+#parse("File Header.java")
+
+@Composable
+fun ${NAME}() {
+}
