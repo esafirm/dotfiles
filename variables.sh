e01a767500f60afaa4adf4453cd789abfbea011b
Esa Firman <esafirm21@gmail.com>
2024-07-09 19:24:16 +0700

    export the variables in variable.sh

--- variables.sh
+++ variables.sh
@@ -1,11 +1,11 @@
 # This file contains global variables that should be exists in every other dotfiles
 
 # dotfile directory
-DOT="$HOME/dotfiles"
+export DOT="$HOME/dotfiles"
 
 # dotfiles directory from chromebook
-CDOT="$DOT/chromebook"
+export CDOT="$DOT/chromebook"
 
 # global bin directories
-BIN="$DOT/bin"
+export BIN="$DOT/bin"
 
