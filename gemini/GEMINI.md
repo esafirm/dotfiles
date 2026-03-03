d9c5cc5a20ffab7cbe99198ae1248c1a90587959
Esa Firman <esafirm21@gmail.com>
2026-03-03 15:28:02 +0700

    Add gemini-cli related dotfiles

---
+++ gemini/GEMINI.md
@@ -0,0 +1,2 @@
+## Gemini Added Memories
+- Always exclude the 'build' directory from searches and file operations.
--- setup-symlink.sh
+++ setup-symlink.sh
@@ -5,3 +5,10 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.warprc ~/.warprc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+## Gemini
+mkdir -p ~/.gemini/extensions
+ln -sf ~/dotfiles/gemini/settings.json ~/.gemini/settings.json
+ln -sf ~/dotfiles/gemini/projects.json ~/.gemini/projects.json
+ln -sf ~/dotfiles/gemini/GEMINI.md ~/.gemini/GEMINI.md
+ln -sf ~/dotfiles/gemini/extensions/extension-enablement.json ~/.gemini/extensions/extension-enablement.json
