8b62df726f514815bd3748f450f451a02d75a96d
Esa Firman <esafirm21@gmail.com>
2026-03-04 13:22:13 +0700

    Fix Gemini Symlinks

--- setup-symlink.sh
+++ setup-symlink.sh
@@ -8,7 +8,8 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 ## Gemini
 mkdir -p ~/.gemini/extensions
-ln -sf ~/dotfiles/gemini/settings.json ~/.gemini/settings.json
-ln -sf ~/dotfiles/gemini/projects.json ~/.gemini/projects.json
-ln -sf ~/dotfiles/gemini/GEMINI.md ~/.gemini/GEMINI.md
-ln -sf ~/dotfiles/gemini/extensions/extension-enablement.json ~/.gemini/extensions/extension-enablement.json
+ln -sf ~/dotfiles/.gemini/policies ~/.gemini/policies
+ln -sf ~/dotfiles/.gemini/settings.json ~/.gemini/settings.json
+ln -sf ~/dotfiles/.gemini/projects.json ~/.gemini/projects.json
+ln -sf ~/dotfiles/.gemini/GEMINI.md ~/.gemini/GEMINI.md
+ln -sf ~/dotfiles/.gemini/extensions/extension-enablement.json ~/.gemini/extensions/extension-enablement.json
